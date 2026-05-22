c91072bf213dd90718b28e22770b14e990bd9c95
Yashraj-singh785 <yashrajsingh7851@gmail.com>
2026-05-22 17:45:41 +0530

    logs and data file are successfully created for data.ingestion.py

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,16 +4,18 @@ from sklearn.model_selection  import train_test_split
 import logging
 import yaml 
 
+# Ensure the "logs" directory exists
 log_dir = "logs"                                  
 os.makedirs(log_dir , exist_ok = True)
 
+# logging configuration
 logger = logging.getLogger('data_ingestion')
 logger.setLevel('DEBUG')
 
-console_handler = logging.streamHandler()
+console_handler = logging.StreamHandler()
 console_handler.setLevel("DEBUG")
 
-log_file_path = os.path.join(log_dir , "data_ingetion.log")
+log_file_path = os.path.join(log_dir , "data_ingestion.log")
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel("DEBUG")
 
@@ -23,10 +25,10 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
-
+"""
 def load_params(params_path: str) -> dict:
      
-    """Load parameters from a YAML file."""
+    '''Load parameters from a YAML file.'''
     
     try:
         with open (params_path , 'r') as file :
@@ -41,7 +43,7 @@ def load_params(params_path: str) -> dict:
         raise
     except Exception as e :
         logger.error("Unexpected error: %s", e)
-        raise
+        raise      """
 
 
 def load_data(data_url :str) ->pd.DataFrame:
@@ -65,7 +67,7 @@ def load_data(data_url :str) ->pd.DataFrame:
         logger.error('Unexepected error occured while loading the dataset : %s' , e)
         raise
 
-def preprocess_data(df : pd.Dataframe ) ->pd.DataFrame:
+def preprocess_data(df : pd.DataFrame ) ->pd.DataFrame:
     """ Preprocessing the data """
     try:
         df.drop(columns =['Unnamed: 2' , 'Unnamed: 3' , 'Unnamed: 4'] , inplace = True)
@@ -85,18 +87,18 @@ def save_data(train_data : pd.DataFrame, test_data: pd.DataFrame, data_path: str
     try:
         raw_data_path = os.path.join(data_path, 'raw')
         os.makedirs(raw_data_path, exist_ok = True)
-        train_data.to_csv(os.path.jooin(raw_data_path,"train.csv"), index = False)
+        train_data.to_csv(os.path.join(raw_data_path,"train.csv"), index = False)
         test_data.to_csv(os.path.join(raw_data_path , "test.csv"), index = False)
-        logger.debug("Train and test data saved to %s" , index = False)
+        logger.debug("Train and test data saved to %s" , data_path)
     except Exception as e :
         logger.error("Unexpected error occured  while saving the data : %s", e)
         raise
 
 def main() :
     try:
-        params =  load_params(params_path = "params.yaml")
-        test_size = params['data_ingestion']['test_size']
-        # test_size =0.2
+        #params =  load_params(params_path = "params.yaml")
+        #test_size = params['data_ingestion']['test_size']
+        test_size =0.2
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
         df = load_data(data_url= data_path)
         final_df = preprocess_data(df)
